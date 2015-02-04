0ed0eb354e0099cfc3b358823aa35c3b6c0203de
lijie.cl <lijie.cl@alibaba-inc.com>
2015-02-04 23:53:32 +0800

    * move GridLayout to library Module

--- app/src/main/java/com/leejay/gridlayout/sample/GridLayoutActivity.java
+++ app/src/main/java/com/leejay/gridlayout/sample/GridLayoutActivity.java
@@ -1,8 +1,10 @@
-package com.leejay.customgridlayout;
+package com.leejay.gridlayout.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.ImageView;
+
+import com.leejay.gridlayout.GridLayout;
 
 /**
  * Created by leejay on 15/02/02.
--- library/src/main/java/com/leejay/gridlayout/GridLayout.java
+++ library/src/main/java/com/leejay/gridlayout/GridLayout.java
@@ -1,4 +1,4 @@
-package com.leejay.customgridlayout;
+package com.leejay.gridlayout;
 
 import android.content.Context;
 import android.content.res.TypedArray;
